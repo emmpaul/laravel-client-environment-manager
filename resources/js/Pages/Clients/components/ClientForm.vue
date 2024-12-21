1e2a01f2b818f17042ec6fbc437cd8e34dbb0e5f
Emmanuel Paul <emmanuel.paul@outlook.com>
2024-12-21 06:59:00 -0500

    fix error when creating client before an instance is created

--- resources/js/Pages/Clients/components/ClientForm.vue
+++ resources/js/Pages/Clients/components/ClientForm.vue
@@ -19,12 +19,12 @@
         <div class="mt-4 flex flex-col gap-4 items-center justify-end">
             <PrimaryButton
                 :class="{ 'opacity-25': form.processing }"
-                :disabled="form.processing || client.name === form.name"
+                :disabled="form.processing || client?.name === form.name"
             >
                 {{ client ? 'Confirm changes' : 'Add a client' }}
             </PrimaryButton>
 
-            <span class="text-sm text-gray-600 dark:text-gray-400" v-if="client.name === form.name">
+            <span class="text-sm text-gray-600 dark:text-gray-400" v-if="client?.name === form.name">
                 <MessageCircleWarning class="inline-block w-5 h-5 mr-2" />
                 {{  `Client name needs to be different from ${client.name}` }}
             </span>
